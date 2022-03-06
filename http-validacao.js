0161ead68f52e263b850b24b599a3ff4b1093ce3
Augusto Monclaro <78120208+monnclaro@users.noreply.github.com>
2022-03-06 03:49:58 -0300

    Update http-validacao.js

--- http-validacao.js
+++ http-validacao.js
@@ -4,14 +4,13 @@ function manejaErros(erro){
     throw new Error(erro.message);
 }
 
-
 async function checaStatus(arrayURLs){
     try {
         const arrayStatus = await Promise.all(arrayURLs.map(async url => {
             const res = await fetch(url)
             return `${res.status} - ${res.statusText}`;;
         }))
-        return arrayStatus;
+    return arrayStatus;
     }
     catch(erro){
         manejaErros(erro)
