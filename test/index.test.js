a606c69e623bd32777787261dfda2884425f9c23
Augusto Monclaro <78120208+monnclaro@users.noreply.github.com>
2022-03-06 03:51:27 -0300

    Update index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,5 @@
 const pegaArquivo = require('../index');
-const jones = require('../index');
 
 test('Deve ser uma função.', () => {
-  expect(typeof jones).toBe('function');
+  expect(typeof pegaArquivo).toBe('function');
 });
